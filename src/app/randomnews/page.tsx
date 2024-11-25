0affb3ca4993b82d0f341a11d0fa11bd9516770f
AMAN KUMAR CHHARI <140314215+chhari07@users.noreply.github.com>
2024-11-25 21:26:15 +0530

    Add files via upload

---
+++ src/app/randomnews/page.tsx
@@ -0,0 +1,15 @@
+import Footer from '@/components/Footer'
+
+import RandomNews from '@/components/RandomNews'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='   bg-black'>
+    <RandomNews/>
+   <Footer/>
+   </div>
+  )
+}
+
+export default page
